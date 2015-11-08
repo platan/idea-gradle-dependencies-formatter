708fc33c5b12121bcb1d2242724c9d24ce5cb868
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2015-11-08 18:53:03 +0100

    More test cases

--- src/test/groovy/com/github/platan/idea/dependencies/gradle/CoordinateComparatorTest.groovy
+++ src/test/groovy/com/github/platan/idea/dependencies/gradle/CoordinateComparatorTest.groovy
@@ -52,12 +52,15 @@ class CoordinateComparatorTest extends Specification {
     def "compare by extension after classifier"() {
         expect:
         Coordinate.parse('g:n:1:e@a') == Coordinate.parse('g:n:1:e@a')
+        Coordinate.parse('g:n:1:@a') == Coordinate.parse('g:n:1:@a')
 
         and:
         Coordinate.parse('g:n:1:e@a') < Coordinate.parse('g:n:1:e@b')
+        Coordinate.parse('g:n:1:@a') < Coordinate.parse('g:n:1:@b')
 
         and:
         Coordinate.parse('g:n:1:e@d') > Coordinate.parse('g:n:1:e@c')
+        Coordinate.parse('g:n:1:@d') > Coordinate.parse('g:n:1:@c')
     }
-    
+
 }
