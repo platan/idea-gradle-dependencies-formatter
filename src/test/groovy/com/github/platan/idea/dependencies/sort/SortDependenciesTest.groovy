c28ddf24c563bb23ae788f4db7018792ccc18bcc
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2015-11-15 18:02:01 +0100

    Test case for sorting dependency list

--- src/test/groovy/com/github/platan/idea/dependencies/sort/SortDependenciesTest.groovy
+++ src/test/groovy/com/github/platan/idea/dependencies/sort/SortDependenciesTest.groovy
@@ -61,6 +61,10 @@ class SortDependenciesTest extends LightCodeInsightTestCase {
         doTest()
     }
 
+    void test__dependency_list() {
+        doTest()
+    }
+
     private doTest() {
         def fileName = getTestName(false).replaceFirst('__', '')
         configureByFile("${fileName}.gradle")
