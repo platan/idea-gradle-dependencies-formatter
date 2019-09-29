4e19040da8db0bf75f0163deffcdbaf45854d2f1
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2019-09-29 19:09:05 +0200

    Format dependencies action added to menu

---
+++ src/main/kotlin/com/github/platan/idea/dependencies/sort/FormatDependenciesAction.kt
@@ -0,0 +1,19 @@
+package com.github.platan.idea.dependencies.sort
+
+import com.github.platan.idea.dependencies.gradle.GradleFileUtil.isGradleFile
+import com.github.platan.idea.dependencies.gradle.GradleFileUtil.isSettingGradle
+import com.intellij.codeInsight.CodeInsightActionHandler
+import com.intellij.codeInsight.actions.CodeInsightAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+
+class FormatDependenciesAction() : CodeInsightAction() {
+
+    override fun getHandler(): CodeInsightActionHandler = FormatDependenciesHandler()
+
+    override fun isValidForFile(project: Project, editor: Editor, file: PsiFile): Boolean {
+        return super.isValidForFile(project, editor, file) && isGradleFile(file) && !isSettingGradle(file)
+    }
+
+}
---
+++ src/main/kotlin/com/github/platan/idea/dependencies/sort/FormatDependenciesHandler.kt
@@ -0,0 +1,14 @@
+package com.github.platan.idea.dependencies.sort
+
+import com.intellij.codeInsight.CodeInsightActionHandler
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+
+class FormatDependenciesHandler : CodeInsightActionHandler {
+
+    override fun invoke(project: Project, editor: Editor, file: PsiFile) {
+    }
+
+    override fun startInWriteAction(): Boolean = false
+}
