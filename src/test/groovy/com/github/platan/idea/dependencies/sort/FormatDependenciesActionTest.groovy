43009569c580ef577c0bd1977901d6849ffa291e
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2019-09-30 18:51:23 +0200

    Test valid files for FormatDependenciesAction

---
+++ src/test/groovy/com/github/platan/idea/dependencies/sort/FormatDependenciesActionTest.groovy
@@ -0,0 +1,28 @@
+package com.github.platan.idea.dependencies.sort
+
+import com.intellij.codeInsight.actions.CodeInsightAction
+import com.intellij.testFramework.fixtures.LightCodeInsightFixtureTestCase
+
+class FormatDependenciesActionTest extends LightCodeInsightFixtureTestCase {
+
+    void test__action_is_not_active_for_settings_gradle() {
+        myFixture.configureByText("settings.gradle", """dependencies {
+    compile 'junit:junit:4.11'
+    compile 'com.google.guava:guava:17.0'
+}""")
+        CodeInsightAction action = new FormatDependenciesAction()
+
+        assert !action.isValidForFile(project, myFixture.editor, myFixture.file)
+    }
+
+    void test__action_is_not_active_for_non_gradle_file() {
+        myFixture.configureByText("test.groovy", """dependencies {
+    compile 'junit:junit:4.11'
+    compile 'com.google.guava:guava:17.0'
+}""")
+        CodeInsightAction action = new FormatDependenciesAction()
+
+        assert !action.isValidForFile(project, myFixture.editor, myFixture.file)
+    }
+
+}
