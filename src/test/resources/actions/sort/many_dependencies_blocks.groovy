bedd92e5a3550e9165b03c6dce1ec071018f11d6
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2020-10-24 16:12:30 +0200

    Test sorting in subprojects block

--- src/test/resources/actions/sort/many_dependencies_blocks.groovy
+++ src/test/resources/actions/sort/many_dependencies_blocks.groovy
@@ -12,6 +12,13 @@ allprojects {
     }
 }
 
+subprojects {
+    dependencies {
+        compile 'junit:junit:4.11'
+        compile 'com.google.guava:guava:18.0'
+    }
+}
+
 dependencies {
     compile 'junit:junit:4.11'
     compile 'com.google.guava:guava:19.0'
--- src/test/resources/actions/sort/many_dependencies_blocks_after.groovy
+++ src/test/resources/actions/sort/many_dependencies_blocks_after.groovy
@@ -12,6 +12,13 @@ allprojects {
     }
 }
 
+subprojects {
+    dependencies {
+        compile 'com.google.guava:guava:18.0'
+        compile 'junit:junit:4.11'
+    }
+}
+
 dependencies {
     compile 'com.google.guava:guava:19.0'
     compile 'junit:junit:4.11'
